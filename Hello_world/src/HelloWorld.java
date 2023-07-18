7bcdb17c633c50cd1497f227f821902dc9eece80
Obmore <bendzsiott1998@gmail.com>
2023-07-18 19:00:40 +0200

    Hello World with sum

--- Hello_world/src/HelloWorld.java
+++ Hello_world/src/HelloWorld.java
@@ -2,7 +2,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");	
+		int a=1,b=3;
+		double sum = a+b;
+		System.out.println("Hello World");
+		System.out.println(sum);;
 	}
 
 }
